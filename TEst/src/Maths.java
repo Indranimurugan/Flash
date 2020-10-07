952dad174f92b0b7f8fe3db8c139dfb5f0e874ff
Raja <Raja@DESKTOP-TNBFJD8>
2020-10-07 20:57:48 +0530

    flash master

---
+++ TEst/src/Maths.java
@@ -0,0 +1,29 @@
+
+public class Maths {
+
+	public static void main(String[] args) {
+		int x =2;
+		int y =3;
+		int z = x+y;
+						
+		System.out.println("sum of T is" + " "+  z);
+		
+		double O = 1.99;
+		
+		int D = (int)O;
+		
+		System.out.println(D);
+		
+		String name ="John";
+		String name1 = "Text";
+	System.out.println(name .concat(" ").concat(name1));
+		
+		for (int i=0; i<5; i++)
+			System.out.println("Yes");
+			
+						
+		
+
+	}
+
+}
